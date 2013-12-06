9013406d84fc2a4841f98c3f5bc025aebc640050
Samuli Seppänen <samuli.seppanen@gmail.com>
2013-12-06 20:05:02 +0200

    Made bacula-fd's monit start/stop rules OS-agnostic
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- manifests/params.pp
+++ manifests/params.pp
@@ -17,6 +17,14 @@ class bacula::params {
             $pid_directory = '/var/run'
             $working_directory = '/var/spool/bacula'
             $ssl_dir = '/etc/bacula/ssl'
+
+            if $::operatingsystem == 'Fedora' {
+                $bacula_filedaemon_service_start = "/usr/bin/systemctl start ${bacula_filedaemon_service}.service"
+                $bacula_filedaemon_service_stop = "/usr/bin/systemctl stop ${bacula_filedaemon_service}.service"
+            } else {
+                $bacula_filedaemon_service_start = "/sbin/service ${bacula_filedaemon_service} start"
+                $bacula_filedaemon_service_stop = "/sbin/service ${bacula_filedaemon_service} stop"
+            }
         }
         'Debian': {
             $bacula_director_package = 'bacula-director-pgsql'
@@ -29,6 +37,8 @@ class bacula::params {
             $pid_directory = '/var/run/bacula'
             $working_directory = '/var/lib/bacula'
             $ssl_dir = '/etc/bacula/ssl'
+            $bacula_filedaemon_service_start = "/usr/sbin/service ${bacula_filedaemon_service} start"
+            $bacula_filedaemon_service_stop = "/usr/sbin/service ${bacula_filedaemon_service} stop"
         }
         'FreeBSD': {
             $bacula_filedaemon_package = 'bacula-client'
@@ -37,6 +47,8 @@ class bacula::params {
             $pid_directory = '/var/run'
             $working_directory = '/var/db/bacula'
             $ssl_dir = '/usr/local/etc/bacula/ssl'
+            $bacula_filedaemon_service_start = "/usr/local/etc/rc.d/${bacula_filedaemon_service} start"
+            $bacula_filedaemon_service_stop = "/usr/local/etc/rc.d/${bacula_filedaemon_service} stop"
         }
         default: {
             $bacula_filedaemon_package = 'bacula-fd'
@@ -45,6 +57,8 @@ class bacula::params {
             $pid_directory = '/var/run/bacula'
             $working_directory = '/var/lib/bacula'
             $ssl_dir = '/etc/bacula/ssl'
+            $bacula_filedaemon_service_start = "/usr/sbin/service ${bacula_filedaemon_service} start"
+            $bacula_filedaemon_service_stop = "/usr/sbin/service ${bacula_filedaemon_service} stop"
         }
     }
 }
