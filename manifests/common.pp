aa5656c4b0f8c15686a2c376a588b6d7dea397e5
Samuli Seppänen <samuli.seppanen@gmail.com>
2015-11-06 14:40:33 +0200

    Automatically add bacula group to prevent failures on the first run
    Signed-off-by: Samuli Seppänen <samuli.seppanen@gmail.com>

--- manifests/common.pp
+++ manifests/common.pp
@@ -5,6 +5,12 @@
 #
 class bacula::common {
 
+    # We need to create the bacula group _before_ installing Bacula, or the 
+    # ::bacula::puppetcerts class will fail on first Puppet run.
+    group { 'bacula':
+        ensure => present,
+    }
+
     class { '::bacula::common::install': }
 
 }
