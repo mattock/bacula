2c130a48376db9b3c39d747212230e1df3585d7b
Samuli Seppänen <samuli@openvpn.net>
2017-10-15 17:54:29 +0300

    Refactor puppet certificate handling
    - Makes the code easier to read
    - Reduces redundancy
    - Add Vagrant tests
    
    Signed-off-by: Samuli Seppänen <samuli@openvpn.net>

--- vagrant/ubuntu-1604-fd.pp
+++ vagrant/ubuntu-1604-fd.pp
@@ -3,3 +3,15 @@
 class { '::monit':
     email => 'root@localhost',
 }
+
+# Ensure that Puppet certificate managements works - these code paths are not
+# exercised when using "puppet apply".
+
+$ssl_dir = '/etc/puppetlabs/puppet/ssl'
+
+file { ["${ssl_dir}/certs/${::fqdn}.pem", "${ssl_dir}/private_keys/${::fqdn}.pem" ]:
+    ensure => 'present',
+}
+
+include ::bacula::common
+include ::bacula::puppetcerts
